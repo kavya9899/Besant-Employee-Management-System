4b396b299c57a9147f6439411f74bbf46d47fae4
kavya <kavyasrinivas2501@gmail.com>
2023-05-08 19:20:01 +0530

    Employee Management

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import App from "./App.js"
+import Reg from "./Registration.js"
+import Emp from "./EmpForm.js"
+import EmpTable from "./EmpTable.js"
+import ReactDOM from "react-dom"
+ReactDOM.render(
+    <App/>,
+    document.getElementById("root")
+)
